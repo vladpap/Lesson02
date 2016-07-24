8434175708e6a65c389d9b30445418761ae957b9
Vladimir Papin <vladimir.papin@gmail.com>
2016-07-25 01:20:39 +0300

    Initial commit

---
+++ src/main/java/ru/sbt/Lesson02.java
@@ -0,0 +1,10 @@
+package ru.sbt;
+
+/**
+ * Created by vladimirpapin on 25.07.16.
+ */
+public class Lesson02 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ src/main/java/ru/sbt/model/Person.java
@@ -0,0 +1,7 @@
+package ru.sbt.model;
+
+/**
+ * Created by vladimirpapin on 25.07.16.
+ */
+public class Person {
+}
