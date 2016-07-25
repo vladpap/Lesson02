435446208c78eef5d2c0b3dd8ca75aa5e9202a74
Vladimir Papin <vladimir.papin@gmail.com>
2016-07-25 12:34:19 +0300

    Done home task 2.

---
+++ src/main/java/ru/sbt/Main.java
@@ -0,0 +1,85 @@
+package ru.sbt;
+
+import ru.sbt.model.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by vladimirpapin on 25.07.16.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        Random random = new Random(System.currentTimeMillis());
+        List<Person> persons = new ArrayList<>();
+        createPersons(persons);
+
+//        printPersons(persons);
+
+
+        int countMaryy = 0;
+        do {
+            Person marryOnePerson = persons.get(random.nextInt(persons.size()));
+            Person marryTwoPerson = persons.get(random.nextInt(persons.size()));
+            System.out.print(marryOnePerson + " сватается " + marryTwoPerson);
+            boolean isMarry = marryOnePerson.marry(marryTwoPerson);
+            if (isMarry) {
+                countMaryy++;
+            }
+            System.out.println(isMarry ? " поженились." : " не получилось.");
+        } while (countMaryy <= 7);
+
+        System.out.println("\nRandom marry ...\n");
+        printPersons(persons);
+        System.out.println("\nRandom divorce ...\n");
+
+        int countDivorce = 0;
+        do {
+            Person divorcePerson = persons.get(random.nextInt(persons.size()));
+            System.out.print(divorcePerson);
+            boolean isDivorce = divorcePerson.divorce();
+            if (isDivorce) {
+                countDivorce++;
+                System.out.println(" Развод.");
+            } else {
+                System.out.println(" Отказ в разводе.");
+            }
+        } while (countDivorce <= 3);
+
+        System.out.println("\nResult\n");
+        printPersons(persons);
+
+    }
+
+    private static void createPersons(List<Person> persons) {
+        persons.add(new Person(("Варвара"), false));
+        persons.add(new Person(("Денис"), true));
+        persons.add(new Person(("Ирина"), false));
+        persons.add(new Person(("Кирилл"), true));
+        persons.add(new Person(("Настасья"), false));
+        persons.add(new Person(("Олег"), true));
+        persons.add(new Person(("Светлана"), false));
+        persons.add(new Person(("Федор"), true));
+        persons.add(new Person(("Юлия"), false));
+        persons.add(new Person(("Михаил"), true));
+        persons.add(new Person(("Василиса"), false));
+        persons.add(new Person(("Ларион"), true));
+        persons.add(new Person(("Анастасия"), false));
+        persons.add(new Person(("Глеб"), true));
+        persons.add(new Person(("Катерина"), false));
+        persons.add(new Person(("Виктор"), true));
+        persons.add(new Person(("Татьяна"), false));
+        persons.add(new Person(("Иван"), true));
+        persons.add(new Person(("Дарья"), false));
+        persons.add(new Person(("Александр"), true));
+    }
+
+    private static void printPersons(List<Person> persons) {
+        for (int i = 0; i < 20; i++) {
+            System.out.println(persons.get(i));
+        }
+    }
+}
--- src/main/java/ru/sbt/model/Person.java
+++ src/main/java/ru/sbt/model/Person.java
@@ -4,4 +4,88 @@ package ru.sbt.model;
  * Created by vladimirpapin on 25.07.16.
  */
 public class Person {
+
+
+    private final boolean man;
+    private final String name;
+    private Person spouse;
+
+    public Person(String name, boolean man) {
+        this.man = man;
+        this.name = name;
+    }
+
+    public boolean isMan() {
+        return man;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Person getSpouse() {
+        return spouse;
+    }
+
+    @Override
+    public String toString() {
+
+        if (this.spouse == null) {
+            return this.name + ((this.man) ? " холост." : " не замужем.");
+        } else {
+            return this.name + ((this.man) ? " женат на " : " замужем за ") + this.spouse.getName() + ".";
+        }
+    }
+
+    /**
+     * This method checks gender of persons. If genders are not equal - tries to marry.
+     * <p>
+     * If one of them has another spouse - execute divorce(sets spouse = null for husband and wife.
+     * Example: if both persons have spouses - then divorce will set 4 spouse to null) and then executes marry().
+     *
+     * @param person - new husband/wife for this person.
+     * @return - returns true if this person has another gender than passed person and they are not husband and wife,
+     * false otherwise
+     */
+    public boolean marry(Person person) {
+
+        if ((person != null) && (person.isMan() != this.man) && (this.spouse != person)) {
+            if (this.spouse != null) {
+                divorseSpouse(this.spouse);
+            }
+            this.spouse = person;
+            if (person.spouse != null) {
+                divorseSpouse(person.spouse);
+            }
+            person.spouse = this;
+
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    /**
+     * Sets spouse = null if spouse is not null
+     *
+     * @return true - if person status has been changed
+     */
+    public boolean divorce() {
+        if (this.spouse == null) {
+            return false;
+        }
+        divorseSpouse(this.spouse);
+        this.spouse = null;
+        return true;
+    }
+
+    /**
+     * Set person.spouse = null
+     *
+     * @param person
+     */
+    private void divorseSpouse(Person person) {
+        person.spouse = null;
+    }
 }
